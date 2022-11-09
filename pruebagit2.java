01c9bd77de029a23b1cb26a6c11d96c6cbe03763
arooiiis <a_aroayanina.arco.ibanez@iespablopicasso.es>
2022-11-09 21:53:52 +0100

    Nuevo programa

---
+++ pruebagit2.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Ejercicio7{
+    public static void main(String[] args){
+    	
+        Scanner sc = new Scanner(System.in);
+        
+        int CANTIDAD =20;
+        
+        int INICIOINTERVALO = 1;
+        
+        int FINALINTERVALO = 10;
+        
+        
+        int [] elementos = new int [CANTIDAD];
+        int [] contador = new int [10];
+        
+        for (int i = 0; i < CANTIDAD; i++) {
+        	
+        	elementos [i] = (int)(Math.random()*(FINALINTERVALO - INICIOINTERVALO +1)) + INICIOINTERVALO;
+        	
+        }
+        for (int i = 0; i < CANTIDAD; i++) {
+        System.out.println(elementos[i]) ;
+        }
+
+        for (int i = 0; i < CANTIDAD; i++) {
+
+          for (int j = 0; j < CANTIDAD; j++) {
+
+            if(elementos[i] == elementos[j]){
+              contador ++;
+            }
+
+
+
+        }
+      }
+
+    }
